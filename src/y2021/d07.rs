be700883898f2fbb5d2cbb1950daef656b1acc48
Steve Mücklisch <steve.muecklisch@gmail.com>
2022-11-06 00:16:58 +0000

    Solve day 7

--- src/y2021/d07.rs
+++ src/y2021/d07.rs
@@ -1,19 +1,77 @@
-//! Day 7
+//! Day 7: The Treachery of Whales
 
 /// part a
-pub fn a(_input: &Vec<&str>) -> String {
-    String::new()
+pub fn a(input: &Vec<&str>) -> String {
+    let mut positions = parse_input(input);
+    positions.sort_unstable();
+    let pivot = positions[positions.len() / 2];
+    let cost = total_cost(&positions, pivot);
+
+    cost.to_string()
 }
 
 /// part b
-pub fn b(_input: &Vec<&str>) -> String {
-    String::new()
+pub fn b(input: &Vec<&str>) -> String {
+    let positions = parse_input(input);
+    let mut min = (usize::MAX, 0);
+    // brute force - O(n²)
+    for i in 0..positions.len() {
+        let cost = total_inc_cost(&positions, i);
+        if cost < min.0 {
+            min = (cost, i);
+        }
+    }
+
+    min.0.to_string()
+}
+
+fn parse_input(input: &Vec<&str>) -> Vec<usize> {
+    input
+        .first()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect()
+}
+
+/// sum of distances to target
+fn total_cost(positions: &Vec<usize>, target: usize) -> usize {
+    positions
+        .iter()
+        .map(|&x| (target as i64 - x as i64).abs() as usize)
+        .sum()
+}
+
+// sum of ascending numbers
+fn to_inc_cost(x: usize) -> usize {
+    x * (x + 1) / 2
+}
+
+fn from_inc_cost(x: usize) -> usize {
+    ((2f64 * x as f64 + 0.25).sqrt() - 0.5).round() as usize
+}
+
+/// sum of incremental fuel costs
+fn total_inc_cost(positions: &Vec<usize>, target: usize) -> usize {
+    positions
+        .iter()
+        .map(|&x| to_inc_cost((target as i64 - x as i64).abs() as usize))
+        .sum()
 }
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input = vec!["16,1,2,0,4,2,7,1,2,14"];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    let positions = parse_input(&input);
+    assert_eq!(positions, vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14]);
+    assert_eq!(total_cost(&positions, 2), 37);
+
+    assert_eq!(to_inc_cost(10), 55);
+    assert_eq!(from_inc_cost(55), 10);
+    assert_eq!(total_inc_cost(&positions, 2), 206);
+    assert_eq!(total_inc_cost(&positions, 5), 168);
+
+    assert_eq!(a(&input), "37");
+    assert_eq!(b(&input), "168");
 }
--- src/y2021/mod.rs
+++ src/y2021/mod.rs
@@ -4,7 +4,7 @@ mod d03;
 mod d04;
 mod d05;
 mod d06;
-// mod d07;
+mod d07;
 // mod d08;
 // mod d09;
 // mod d10;
@@ -40,8 +40,8 @@ pub fn solve(day: u8, part: PartValues, input: &Vec<&str>) -> String {
         (05, B) => d05::b(&input),
         (06, A) => d06::a(&input),
         (06, B) => d06::b(&input),
-        // (07, A) => d07::a(&input),
-        // (07, B) => d07::b(&input),
+        (07, A) => d07::a(&input),
+        (07, B) => d07::b(&input),
         // (08, A) => d08::a(&input),
         // (08, B) => d08::b(&input),
         // (09, A) => d09::a(&input),
