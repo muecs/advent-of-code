4dd80305e4ebff5ad7fc949813ad53bcac9ba349
Steve Mücklisch <steve.muecklisch@gmail.com>
2022-12-03 22:56:11 +0000

    Solve Day 1: Report Repair (2020)

--- src/y2020/d01.rs
+++ src/y2020/d01.rs
@@ -1,19 +1,56 @@
-//! Day 1
+//! Day 1: Report Repair
 
-/// part a
-pub fn a(_input: &Vec<&str>) -> String {
+const TARGET_SUM: usize = 2020;
+
+/// product of the two entries that sum to 2020
+pub fn a(input: &Vec<&str>) -> String {
+    let numbers = parse_input(input);
+    for i in 0..numbers.len()-1 {
+        for j in i+1..numbers.len() {
+            let n1 = &numbers[i];
+            let n2 = &numbers[j];
+            if *n1 + *n2 == TARGET_SUM {
+                return (*n1 * *n2).to_string();
+            }
+        }
+    }
     String::new()
 }
 
-/// part b
-pub fn b(_input: &Vec<&str>) -> String {
+/// product of the three entries that sum to 2020
+pub fn b(input: &Vec<&str>) -> String {
+    let numbers = parse_input(input);
+    for i in 0..numbers.len()-2 {
+        for j in i+1..numbers.len()-1 {
+            for k in j+1..numbers.len() {
+                let n1 = &numbers[i];
+                let n2 = &numbers[j];
+                let n3 = &numbers[k];
+                if *n1 + *n2 + *n3 == TARGET_SUM {
+                    return (*n1 * *n2 * *n3).to_string();
+                }
+            }
+        }
+    }
+    
     String::new()
+}
+
+fn parse_input(input: &Vec<&str>) -> Vec<usize> {
+    input.iter().map(|s| s.parse().unwrap()).collect()
 }
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input = vec![
+        "1721",
+        "979",
+        "366",
+        "299",
+        "675",
+        "1456",
+    ];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    assert_eq!(a(&input), "514579");
+    assert_eq!(b(&input), "241861950");
 }
