4217e1f9d40c8c5447efd1ff3f83d877f87bd76b
Steve Mücklisch <steve.muecklisch@gmail.com>
2022-12-03 12:30:25 +0000

    Solve Day 3: Rucksack Reorganization

--- src/y2022/d03.rs
+++ src/y2022/d03.rs
@@ -1,19 +1,67 @@
-//! Day 3
+//! Day 3: Rucksack Reorganization
 
-/// part a
-pub fn a(_input: &Vec<&str>) -> String {
-    String::new()
+use std::collections::BTreeSet;
+
+/// sum of priorities of items in both halves of each string
+pub fn a(input: &Vec<&str>) -> String {
+    input
+        .iter()
+        .map(|s| find_shared_item(s))
+        .map(|c| get_item_priority(c))
+        .sum::<usize>()
+        .to_string()
 }
 
-/// part b
-pub fn b(_input: &Vec<&str>) -> String {
-    String::new()
+/// sum of priorities of shared item per group of 3
+pub fn b(input: &Vec<&str>) -> String {
+    input
+        .chunks_exact(3)
+        .map(|group| {
+            group
+                .iter()
+                .map(|s| BTreeSet::from_iter(s.chars()))
+                .reduce(|acc, set| BTreeSet::from_iter(acc.intersection(&set).cloned()))
+                .unwrap()
+                .iter()
+                .next()
+                .map(|c| get_item_priority(*c))
+                .unwrap()
+        })
+        .sum::<usize>()
+        .to_string()
+}
+
+fn find_shared_item(s: &str) -> char {
+    let (a, b) = s.split_at(s.len() / 2);
+    let set_a = BTreeSet::from_iter(a.chars());
+    let set_b = BTreeSet::from_iter(b.chars());
+    *set_a.intersection(&set_b).next().unwrap()
+}
+
+fn get_item_priority(c: char) -> usize {
+    let b = c as u8;
+    match b {
+        b'a'..=b'z' => (b - b'a' + 1).into(),
+        b'A'..=b'Z' => (b - b'A' + 27).into(),
+        _ => unreachable!("invalid item"),
+    }
 }
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input = vec![
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+    ];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    assert_eq!(find_shared_item(input[0]), 'p');
+    assert_eq!(get_item_priority('p'), 16);
+    assert_eq!(get_item_priority('L'), 38);
+
+    assert_eq!(a(&input), "157");
+    assert_eq!(b(&input), "70");
 }
