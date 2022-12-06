76b12230b115c8eb72a99ac9a660207bd3c732a1
Steve Mücklisch <steve.muecklisch@gmail.com>
2022-12-06 21:12:38 +0000

    Solve Day 6: Tuning Trouble (2022)

--- src/y2022/d06.rs
+++ src/y2022/d06.rs
@@ -1,19 +1,52 @@
-//! Day 6
+//! Day 6: Tuning Trouble
 
-/// part a
-pub fn a(_input: &Vec<&str>) -> String {
-    String::new()
+/// end of first sequence of 4 unique characters
+pub fn a(input: &Vec<&str>) -> String {
+    find_marker(input[0], 4).unwrap().to_string()
 }
 
-/// part b
-pub fn b(_input: &Vec<&str>) -> String {
-    String::new()
+/// end of first sequence of 14 unique characters
+pub fn b(input: &Vec<&str>) -> String {
+    find_marker(input[0], 14).unwrap().to_string()
+}
+
+fn find_marker(msg: &str, len: usize) -> Option<usize> {
+    let mut index = len;
+    for window in msg.as_bytes().windows(len) {
+        let mut is_repeated = false;
+        'outer: for i in 0..len-1 {
+            for j in i+1..len {
+                if window[i] == window[j] {
+                    is_repeated = true;
+                    break 'outer;
+                }
+            }
+        }
+        if !is_repeated {
+            return Some(index);
+        }
+        index += 1;
+    }
+    None
 }
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input1 = vec!["mjqjpqmgbljsphdztnvjfqwrcgsmlb"];
+    let input2 = vec!["bvwbjplbgvbhsrlpgdmjqwftvncz"];
+    let input3 = vec!["nppdvjthqldpwncqszvftbrmjlhg"];
+    let input4 = vec!["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"];
+    let input5 = vec!["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    assert_eq!(a(&input1), "7");
+    assert_eq!(a(&input2), "5");
+    assert_eq!(a(&input3), "6");
+    assert_eq!(a(&input4), "10");
+    assert_eq!(a(&input5), "11");
+
+    assert_eq!(b(&input1), "19");
+    assert_eq!(b(&input2), "23");
+    assert_eq!(b(&input3), "23");
+    assert_eq!(b(&input4), "29");
+    assert_eq!(b(&input5), "26");
 }
