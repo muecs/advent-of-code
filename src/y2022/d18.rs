de1b2ca9ddaa2bf40a68aac7e702a0425b3515d0
Steve Mücklisch <steve.muecklisch@gmail.com>
2022-12-19 00:50:40 +0000

    Solve Day 18: Boiling Boulders (2022)

--- src/y2022/d18.rs
+++ src/y2022/d18.rs
@@ -1,19 +1,102 @@
-//! Day 18
+//! Day 18: Boiling Boulders
 
-/// part a
-pub fn a(_input: &Vec<&str>) -> String {
-    String::new()
+use std::collections::BTreeSet;
+
+/// surface area of voxel shape
+pub fn a(input: &Vec<&str>) -> String {
+    let voxels = parse_input(input);
+    voxels
+        .iter()
+        .map(|p| 6 - count_adjacent(p, &voxels))
+        .sum::<usize>()
+        .to_string()
 }
 
-/// part b
-pub fn b(_input: &Vec<&str>) -> String {
-    String::new()
+/// exterior surface area of voxel shape
+pub fn b(input: &Vec<&str>) -> String {
+    let voxels = parse_input(input);
+    let (min_voxel, max_voxel) = voxels.iter().fold(((i8::MAX, i8::MAX, i8::MAX), (0, 0, 0)), |(acc_min, acc_max), p| {
+        (
+            (acc_min.0.min(p.0), acc_min.1.min(p.1), acc_min.2.min(p.2)),
+            (acc_max.0.max(p.0), acc_max.1.max(p.1), acc_max.2.max(p.2))
+        )
+    });
+    let mut outer_voxels = Voxels::new();
+    let mut unvisited = vec![(min_voxel.0 - 1, min_voxel.1 - 1, min_voxel.2 - 1)];
+    while let Some(p) = unvisited.pop() {
+        if voxels.contains(&p) || outer_voxels.contains(&p) {
+            continue;
+        }
+        outer_voxels.insert(p);
+        let adjacent = get_adjacent(&p);
+        for adj in &adjacent {
+            if adj.0 >= min_voxel.0 - 1
+                && adj.0 <= max_voxel.0 + 1
+                && adj.1 >= min_voxel.1 - 1
+                && adj.1 <= max_voxel.1 + 1
+                && adj.2 >= min_voxel.2 - 1
+                && adj.2 <= max_voxel.2 + 1
+            {
+                unvisited.push(*adj);
+            }
+        }
+    }
+
+    voxels
+        .iter()
+        .map(|p| count_adjacent(p, &outer_voxels))
+        .sum::<usize>()
+        .to_string()
 }
+
+fn parse_input(input: &Vec<&str>) -> Voxels {
+    input
+        .iter()
+        .map(|line| {
+            let mut it = line.split(',').map(|s| s.parse().unwrap());
+            (it.next().unwrap(), it.next().unwrap(), it.next().unwrap())
+        })
+        .collect()
+}
+
+fn get_adjacent(p: &Point) -> Vec<Point> {
+    const OFFSETS: [i8; 2] = [-1, 1];
+    let mut points = Vec::new();
+    for x in OFFSETS {
+        points.push((p.0 + x, p.1, p.2));
+    }
+    for y in OFFSETS {
+        points.push((p.0, p.1 + y, p.2));
+    }
+    for z in OFFSETS {
+        points.push((p.0, p.1, p.2 + z));
+    }
+    points
+}
+
+fn count_adjacent(p: &Point, voxels: &Voxels) -> usize {
+    get_adjacent(p)
+        .iter()
+        .filter(|adj| voxels.contains(adj))
+        .count()
+}
+
+type Point = (i8, i8, i8);
+type Voxels = BTreeSet<Point>;
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input = vec![
+        "2,2,2", "1,2,2", "3,2,2", "2,1,2", "2,3,2", "2,2,1", "2,2,3", "2,2,4", "2,2,6", "1,2,5",
+        "3,2,5", "2,1,5", "2,3,5",
+    ];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    let test_input = Voxels::from([(1, 1, 1), (2, 1, 1)]);
+    assert_eq!(count_adjacent(&(1, 1, 1), &test_input), 1);
+    assert_eq!(count_adjacent(&(2, 1, 1), &test_input), 1);
+    assert_eq!(count_adjacent(&(3, 1, 1), &test_input), 1);
+    assert_eq!(count_adjacent(&(2, 2, 2), &test_input), 0);
+
+    assert_eq!(a(&input), "64");
+    assert_eq!(b(&input), "58");
 }
--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -15,7 +15,7 @@ mod d14;
 mod d15;
 mod d16;
 // mod d17;
-// mod d18;
+mod d18;
 // mod d19;
 // mod d20;
 // mod d21;
@@ -62,8 +62,8 @@ pub fn solve(day: u8, part: PartValues, input: &Vec<&str>) -> String {
         (16, B) => d16::b(&input),
         // (17, A) => d17::a(&input),
         // (17, B) => d17::b(&input),
-        // (18, A) => d18::a(&input),
-        // (18, B) => d18::b(&input),
+        (18, A) => d18::a(&input),
+        (18, B) => d18::b(&input),
         // (19, A) => d19::a(&input),
         // (19, B) => d19::b(&input),
         // (20, A) => d20::a(&input),
