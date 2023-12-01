0f829a4fbc4a7aa7e64bf5bd47f52bf7278c4f8e
Steve Mücklisch <steve.muecklisch@gmail.com>
2023-12-01 08:15:51 +0000

    Solve Day 1: Trebuchet?! (2023)

--- src/y2023/d01.rs
+++ src/y2023/d01.rs
@@ -1,21 +1,56 @@
-//! Day 1
+//! Day 1: Trebuchet?!
 
 /// part a
-pub fn a(_input: &Vec<&str>) -> String {
-    String::new()
+pub fn a(input: &Vec<&str>) -> String {
+    input
+        .iter()
+        .map(|s| {
+            // print!("{}", s);
+            let mut s2 = s
+                .chars()
+                .filter_map(|c| (c >= '0' && c <= '9').then_some(c))
+                .collect::<String>();
+            s2 = s2[0..1].to_string() + &s2[s2.len() - 1..].to_string();
+            // println!(" -> {}", s2);
+            s2.parse::<usize>().unwrap()
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 /// part b
-pub fn b(_input: &Vec<&str>) -> String {
-    String::new()
+pub fn b(input: &Vec<&str>) -> String {
+    let processed = input
+        .iter()
+        .map(|s| {
+            // allow for overlapping digit words
+            s.replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "4")
+                .replace("five", "5e")
+                .replace("six", "6")
+                .replace("seven", "7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e")
+        })
+        .collect::<Vec<_>>();
+    a(&processed.iter().map(|s| &**s).collect())
 }
-
-// fn parse_input(input: &Vec<&str>) {}
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input1 = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+    let input2 = vec![
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    assert_eq!(a(&input1), "142");
+    assert_eq!(b(&input2), "281");
 }
--- src/y2023/mod.rs
+++ src/y2023/mod.rs
@@ -1,4 +1,4 @@
-// mod d01;
+mod d01;
 // mod d02;
 // mod d03;
 // mod d04;
@@ -28,8 +28,8 @@ use crate::args::PartValues::{self, *};
 
 pub fn solve(day: u8, part: PartValues, input: &Vec<&str>) -> String {
     match (day, part) {
-        // (01, A) => d01::a(&input),
-        // (01, B) => d01::b(&input),
+        (01, A) => d01::a(&input),
+        (01, B) => d01::b(&input),
         // (02, A) => d02::a(&input),
         // (02, B) => d02::b(&input),
         // (03, A) => d03::a(&input),
