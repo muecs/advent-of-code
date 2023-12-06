9e183653a357cabc3f43ae0ff951951706d49ab2
Steve Mücklisch <steve.muecklisch@gmail.com>
2023-12-06 10:40:20 +0000

    Solve Day 6: Wait For It (2023)

--- src/y2023/d06.rs
+++ src/y2023/d06.rs
@@ -1,21 +1,51 @@
-//! Day 6
+//! Day 6: Wait For It
 
-/// part a
-pub fn a(_input: &Vec<&str>) -> String {
-    String::new()
+/// number of ways to beat the record (multiple races)
+pub fn a(input: &Vec<&str>) -> String {
+    let times_it = input[0][9..]
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<usize>().unwrap());
+    let distances_it = input[1][9..]
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<usize>().unwrap());
+    times_it
+        .zip(distances_it)
+        .map(|(time, distance)| count_winning_timings(time, distance))
+        .product::<usize>()
+        .to_string()
 }
 
-/// part b
-pub fn b(_input: &Vec<&str>) -> String {
-    String::new()
+/// number of ways to beat the record (single race)
+pub fn b(input: &Vec<&str>) -> String {
+    let time = input[0][9..]
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+    let distance = input[1][9..]
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+    count_winning_timings(time, distance).to_string()
 }
 
-// fn parse_input(input: &Vec<&str>) {}
+fn count_winning_timings(time: usize, distance: usize) -> usize {
+    // d_min = (t_max - t) * t
+    let t_max = time as f64;
+    let d_min = distance as f64;
+    let r = (t_max * t_max - 4.0 * d_min).sqrt();
+    let low = ((t_max - r) / 2.0).floor() as usize + 1;
+    let high = ((t_max + r) / 2.0).ceil() as usize - 1;
+    high + 1 - low
+}
 
 #[test]
 pub fn test() {
-    let input = vec![];
+    let input = vec!["Time:      7  15   30", "Distance:  9  40  200"];
 
-    assert_eq!(a(&input), "");
-    assert_eq!(b(&input), "");
+    assert_eq!(a(&input), "288");
+    assert_eq!(b(&input), "71503");
 }
